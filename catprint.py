d047b7d88f733cec3ed7102dd7dac40e64d327b6
LT <larisa.turlova@gmail.com>
2023-03-28 11:57:40 +0300

    Create catprint.py

---
+++ catprint.py
@@ -0,0 +1,9 @@
+from cat import Cat
+
+cat1 = Cat('Барон','мальчик',2)
+cat2 = Cat('Сэм','мальчик',2)
+cat3 = Cat('Bыдуманный','кот',6)
+
+print(f'Ya {cat1.name}, {cat1.gender}, mne {cat1.age}')
+print(f'A ya - {cat2.gender} {cat2.name}, mne {cat2.age}')
+print(f'i byl by ya - {cat3.name} dlya rznoobraziya {cat3.gender}, mne {cat3.age}, no menya net')
